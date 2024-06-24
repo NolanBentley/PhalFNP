b003f129c4b528a9069cae28a96810e88dae71e2
NolanBentley <NolanBentley@gmail.com>
2024-06-24 12:45:25 -0500

    Aggregate was failing to work right only when in parralel mode. No idea why. Rewrote aggFun using data.table to fix it.

--- scripts/depthToAggregate_FNP.R
+++ scripts/depthToAggregate_FNP.R
@@ -4,30 +4,30 @@ depthDir <- "./data_ignored/secondary/depths"
 outDir   <- "./data/"
 prelimFile <- paste0("prelimAggregateDepths_",format(Sys.time(), "%Y%m%d_%H%M"),".csv")
 totalSteps <- 5
-nCores     <- min(c(20,detectCores()/2))
+nCores     <- min(c(10,parallel::detectCores()/2))
 
 #Setup the environment
 setwd(wd)
-library(data.table)
 library(parallel)
 
 ## Load functions
 # Calculate the mean depth per window (byLen)
 source('./scripts/aggFun1.R')
 
-
 #Get data
 depthFileList <- lapply(1:totalSteps,function(x){list.files(depthDir,pattern = paste0(x,"of",totalSteps,"int.depth\\.out$"),full.names = T)})
 cl <- makeCluster(nCores)
-stopCluster(cl)
+j<-1
 for(j in 1:length(depthFileList)){
   if(j==1){aggDf <- NULL}
   byLen <- NULL #Triggers aggFun1 to recalculate each step
   cat("\n\n\n",j,"\n")
   currDepthFiles <- depthFileList[[j]]
-  currDepthFiles <- currDepthFiles[1:40]
-  parOut <- parallel::parSapply(currDepthFiles,cl,function(x){cbind(j,i,aggFun1(currDepthFiles,byLen=byLen))})
-  aggDf<- list(aggDf,parOut)
+  #currDepthFiles<-currDepthFiles[1:40]
+  parOut   <- parallel::parSapply(cl,currDepthFiles,aggFun2,simplify = F)
+  parOutDf <- do.call("rbind",parOut)
+  #View(parOutDf)
+  aggDf <- rbind(aggDf,parOut)
 }
 write.csv(aggDf,file = file.path(outDir,prelimFile))
 stopCluster(cl)
