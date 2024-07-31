d755f0d2d230432f21eca6cf7bda0dcddfd5450b
Nolan Bentley <nolanbentley@gmail.com>
2024-07-31 12:20:06 -0500

    Renamed key objects to prevent confusion when working in pipeline. Rewrote to match new interval

--- scripts/06_buildWebsiteIndex.R
+++ scripts/06_buildWebsiteIndex.R
@@ -1,19 +1,25 @@
+###Setup####
 #Set variables
 wd <- "~/Experiments/PhalFNP/"
-aggFile <- "data_ignored/secondary/plottedAggDf_n.csv"
+winFile <- "data_ignored/secondary/windowedNs_7x4.999kbx23.75kb(~147.499Kb).csv"
+toggleFilterToChr      <- T
 toggleFilterToExisting <- T
 
-#### Load aggDf ####
+#Set dir
 setwd(wd)
-if(!exists("aggDf")){aggDf<-read.csv(aggFile)}
-aggDf_og <-aggDf
-aggDf<-aggDf[aggDf$chrLogic,]
+
+#### Load winDf ####
+if(!exists("winDf")){winDf<-read.csv(winFile)}
+winDf2 <- winDf
+if(toggleFilterToChr){
+  winDf2 <- winDf2[winDf2$chrLogic,]
+}
 
 #Remove files if desired
 if(toggleFilterToExisting){
-  dirsToSearch <- unique(dirname(unique(aggDf$singleLineSingleChr)))
+  dirsToSearch <- unique(dirname(unique(winDf2$singleLineSingleChr)))
   currFiles <- unique(unlist(sapply(dirsToSearch,list.files)))
-  aggDf <- aggDf[basename(aggDf$singleLineSingleChr)%in%currFiles,]
+  winDf2 <- winDf2[basename(winDf2$singleLineSingleChr)%in%currFiles,]
 }
 
 #### Build index ####
@@ -31,8 +37,9 @@ Panicum hallii fast-neutron population large structural variant explorer
 This website houses interactive plots for exploring large (>>0.2Mb) copy-number variants from the <i>Panicum. hallii</i> fast neutron population (FNP).<br>
 <br>
 <dt>The y-axis values were calculated to approximate ploidy across the x-axis intervals:</dt>
-<dd>- Each point is the transformed median copy-number across a 5x window of 5Kb intervals every 40Kb for an individual line.</dt>
+<dd>- Each point is the transformed median copy-number across a 7x window of 5Kb intervals every 23.75Kb for an individual line.</dt>
 <dd>- Each interval summarizes the mean read depth across a 2.5-5kb range of positions excluding positions with 0 depth.</dd>
+<dd>- This results in each median value being calculated from the mean coverage across 7 regions spanning approximetly 147.5Kb or larger.</dd>
 <dd>- The "copy-number" was estimated by multiplying the mean coverage across the interval by 2 and dividing by the peak mean coverage.</dd><br>
 <dt>This data is visualized with two primary statistics:</dt>
 <dd>- <b>Hexagons:</b> the frequency of bins of position and copy number across the population being visualized.<br></dd>
@@ -44,18 +51,18 @@ This website houses interactive plots for exploring large (>>0.2Mb) copy-number 
 ')
 index<-unlist(strsplit(index,"\n"))
 index_endBody <- which(trimws(index)=="</body>")
-aggDf$singleLineBinPages <- paste0("./depthImages/singleLineIndexes/",aggDf$idNum_bin,".html")
-singleLineBinPages <-sort(unique(aggDf$singleLineBinPages))
+winDf2$singleLineBinPages <- paste0("./depthImages/singleLineIndexes/",winDf2$idNum_bin,".html")
+singleLineBinPages <-sort(unique(winDf2$singleLineBinPages))
 index_added <- c(
   "<br><br><b><u>multi-sample multi-chromosome plots</b></u>",
   "<br>Clicking on points in these plots will take you to that chromosome's multi-sample plot.",
   "<br>Note: These are large (~40Mb) files and will take a bit to load and render.",
   "<ul>",
-  sort(unique(aggDf$multiLineMultiChr)),
+  sort(unique(winDf2$multiLineMultiChr)),
   "</ul><br><br><b><u>multi-sample single-chromosome plots</b></u>",
   "<br>Clicking on points in these plots will take you to that sample's multi-chromosome plot. Clicking on those points will take you to the single-chromosome plot for that line.",
   "<ul>",
-  sort(unique(aggDf$multiLineSingleChr)),
+  sort(unique(winDf2$multiLineSingleChr)),
   "</ul><br><br><b><u>Single line plots</b></u>",
   "This section takes you to single-sample plots based on the sample ID.",
   "<ul>",
@@ -90,16 +97,16 @@ for(i in 1:length(posHtmlFiles)){
     '</p>','</body>','</html>'
   )
   index_endBody <- which(trimws(index)=="</body>")
-  currRows <- aggDf$singleLineBinPages == posHtmlFiles[i]
+  currRows <- winDf2$singleLineBinPages == posHtmlFiles[i]
   index_added <- c(
     "<br><br><b><u>single-sample multi-chromosome plots</b></u>",
     "<br>Clicking on points in these plots will take you to that chromosome's single-sample plot.",
     "<ul>",
-    sort(unique(aggDf$singleLineMultiChr[currRows])),
+    sort(unique(winDf2$singleLineMultiChr[currRows])),
     "</ul><br><br><b><u>single-sample single-chromosome plots</b></u>",
     "<br>Clicking on points in these plots will take you to that sample's multi-chromosome plot. Clicking on those points will take you to the single-chromosome plot for that line.",
     "<ul>",
-    sort(unique(aggDf$singleLineSingleChr[currRows]))
+    sort(unique(winDf2$singleLineSingleChr[currRows]))
   )
   htmlPos <-grep("html",index_added)
   index_mod <- index_added
